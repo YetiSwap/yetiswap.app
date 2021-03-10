5bd2661622454579807c38fd05b164dcd0c0fe63
mutlufuruncu <mutlufuruncu@gmail.com>
2021-03-10 13:21:16 +0300

    change volume and liquidity separator

--- src/utils/priceToString.js
+++ src/utils/priceToString.js
@@ -1,6 +1,7 @@
 export default function priceToString(value) {
     const suffixes = ["", "K", "M", "B", "T"];
-    const suffixNum = Math.floor(("" + value).length / 4);
+    const priceLength = ("" + value).length;
+    const suffixNum = Math.floor(priceLength / (priceLength === 6 ? 4 : 3));
     let shortValue = parseFloat((suffixNum!==0 ? (value / Math.pow(1000, suffixNum)):value).toPrecision(2));
     if (shortValue % 1!==0) {
         shortValue = shortValue.toFixed(1);
